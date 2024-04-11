9f893da4580b8dcc0bf5851fc4ebe254c1678b14
David Venegas <venegasdavidm@gmail.com>
2024-04-10 19:04:57 -0700

    fixing the year

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const hoursEl = document.getElementById('hours')
 const minsEl = document.getElementById('mins')
 const secondsEl = document.getElementById('seconds')
 
-const newYears = '1 Jan 2023'
+const thisYear = new Date().getFullYear()
+const newYears = new Date(`January 01 ${thisYear + 1} 00:00:00`)
 
 function countdown() {
     const newYearsDate = new Date(newYears)
     const currentDate = new Date()
     const totalSeconds = new Date(newYearsDate - currentDate) / 1000
-    const days =  Math.floor(totalSeconds / 3600 / 24)
+    const days = Math.floor(totalSeconds / 3600 / 24)
     const hours = Math.floor(totalSeconds / 3600) % 24
     const mins = Math.floor(totalSeconds / 60) % 60
     const seconds = Math.floor(totalSeconds) % 60
